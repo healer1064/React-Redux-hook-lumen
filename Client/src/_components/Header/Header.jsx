d405c342a9236f1b6f02688b558aa5d7b748d42c
healer1064 <healer1064@outlook.com>
2021-07-15 06:41:44 +0300

    Lumen skeleton is made. React uses Lumen APIs.

---
+++ Client/src/_components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/usersList">Users List</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/editProfile">Edit profile</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Logout</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export { Header };
---
+++ Server/README.md
@@ -0,0 +1,9 @@
+# lumen-auth-example
+
+## Usage
+
+-   `composer install`
+-   `cp .env.example .env (edit it with your database information)`
+-   `php artisan jwt:secret`
+-   `php artisan migrate`
+-   `php -S localhost:8000 -t public`
